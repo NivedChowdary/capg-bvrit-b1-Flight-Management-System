c2e6c7fcb3e884e63456ed9c041322145e8a849e
Nived Chowdary <nivedchoudary@gmail.com>
2020-04-22 20:07:59 +0530

    2nd commit-fixed errors

--- fms-schedule-flights/src/main/java/com/capg/fms/scheduleflights/model/Passenger.java
+++ fms-schedule-flights/src/main/java/com/capg/fms/scheduleflights/model/Passenger.java
@@ -4,49 +4,56 @@ import java.math.BigInteger;
 
 public class Passenger {
 
-	private BigInteger pnrNumber; 
+	private BigInteger pnrNumber;
 	private String passengerName;
 	private int passengerAge;
 	private BigInteger passengerUIN;
-	private double Luggage;
-	
-	
+	private double luggage;
+
 	public BigInteger getPnrNumber() {
 		return pnrNumber;
 	}
+
 	public void setPnrNumber(BigInteger pnrNumber) {
 		this.pnrNumber = pnrNumber;
 	}
+
 	public String getPassengerName() {
 		return passengerName;
 	}
+
 	public void setPassengerName(String passengerName) {
 		this.passengerName = passengerName;
 	}
+
 	public int getPassengerAge() {
 		return passengerAge;
 	}
+
 	public void setPassengerAge(int passengerAge) {
 		this.passengerAge = passengerAge;
 	}
+
 	public BigInteger getPassengerUIN() {
 		return passengerUIN;
 	}
+
 	public void setPassengerUIN(BigInteger passengerUIN) {
 		this.passengerUIN = passengerUIN;
 	}
+
 	public double getLuggage() {
-		return Luggage;
+		return luggage;
 	}
+
 	public void setLuggage(double luggage) {
-		Luggage = luggage;
+		this.luggage = luggage;
 	}
+
 	@Override
 	public String toString() {
 		return "Passenger [pnrNumber=" + pnrNumber + ", passengerName=" + passengerName + ", passengerAge="
-				+ passengerAge + ", passengerUIN=" + passengerUIN + ", Luggage=" + Luggage + "]";
+				+ passengerAge + ", passengerUIN=" + passengerUIN + ", Luggage=" + luggage + "]";
 	}
-	
-	
-	
+
 }
