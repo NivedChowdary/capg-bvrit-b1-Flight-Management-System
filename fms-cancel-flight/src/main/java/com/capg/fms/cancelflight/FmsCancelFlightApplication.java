a1502e9bacaaaf4c7dbccceeef3c1dd09a8ba9b2
meghana <16211A05N5@bvrit.ac.in>
2020-04-23 14:21:13 +0530

    Added Entities

---
+++ fms-cancel-flight/src/main/java/com/capg/fms/cancelflight/FmsCancelFlightApplication.java
@@ -0,0 +1,13 @@
+package com.capg.fms.cancelflight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmsCancelFlightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmsCancelFlightApplication.class, args);
+	}
+
+}
