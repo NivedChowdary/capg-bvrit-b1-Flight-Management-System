65051f7c37f2842eaa356f5105d3daa3b49e6b9c
Welcome <Welcome@192.168.100.10>
2020-04-26 00:38:09 +0530

    login and create account basic service implementation added

--- fms-login-ms/src/main/java/com/capg/fms/login/FmsLoginMsApplication.java
+++ fms-login-ms/src/main/java/com/capg/fms/login/FmsLoginMsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FmsLoginModuleApplication {
+public class FmsLoginMsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FmsLoginModuleApplication.class, args);
+		SpringApplication.run(FmsLoginMsApplication.class, args);
 	}
 
 }
