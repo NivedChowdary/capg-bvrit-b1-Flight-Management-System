9a70e535a5f37a24396dc69201766129181b7b8b
Nived Chowdary <nivedchoudary@gmail.com>
2020-04-27 08:36:01 +0530

    Added Controller

--- fms-schedule-ms/src/main/java/com/capg/fms/schedule/controller/ScheduleController.java
+++ fms-schedule-ms/src/main/java/com/capg/fms/schedule/controller/ScheduleController.java
@@ -1,14 +1,59 @@
 package com.capg.fms.schedule.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.capg.fms.schedule.model.Schedule;
+import com.capg.fms.schedule.model.ScheduledFlight;
 import com.capg.fms.schedule.service.ScheduleServiceImpl;
 
 @RestController
+@RequestMapping("/scheduleflight")
 public class ScheduleController {
 	@Autowired
 	ScheduleServiceImpl service;
-	
 
+	@PostMapping("/addscheduledflight")
+	public ScheduledFlight addScheduleFlight(ScheduledFlight scheduledFlight) {
+		ScheduledFlight scheduleFlight1 = service.addScheduleFlight(scheduledFlight);
+		return scheduleFlight1;
+	}
+
+	@GetMapping("/getscheduledflight{scheduleId}")
+	public ScheduledFlight viewScheduledFlight(@PathVariable int scheduleId) {
+
+		return service.viewScheduledFlight(scheduleId);
+
+	}
+
+	@GetMapping(value = "/getallscheduledflight")
+	public List<ScheduledFlight> viewScheduledFlight() {
+
+		return service.viewScheduledFlight();
+
+	}
+
+	@PostMapping("/modifyscheduledflight")
+	public ScheduledFlight modifyScheduledFlight(@PathVariable long flightNumber, @RequestBody Schedule schedule,
+			@PathVariable int scheduleId) {
+
+		ScheduledFlight scheduledFlight = service.modifyScheduledFlight(flightNumber, schedule, scheduleId);
+
+		return scheduledFlight;
+
+	}
+
+	@DeleteMapping("/deletescheduledflight/{scheduleid}")
+	public String deleteScheduledFlight(@PathVariable int scheduleId) {
+		service.deleteScheduledFlight(scheduleId);
+		return "Flight schedule deleted";
+	}
 }
