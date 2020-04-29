115062abf39da9e931722247141226ffb7537e67
karth <karth@192.168.2.6>
2020-04-29 16:07:25 +0530

    Junit cases added

--- fms-schedule-ms/src/test/java/com/capg/fms/schedule/FmsScheduleMsApplicationTests.java
+++ fms-schedule-ms/src/test/java/com/capg/fms/schedule/FmsScheduleMsApplicationTests.java
@@ -1,12 +1,13 @@
 package com.capg.fms.schedule;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import com.capg.fms.schedule.model.ScheduledFlight;
-import com.capg.fms.schedule.repository.IAvailabilityScheduleRepo;
-import com.capg.fms.schedule.repository.IAvailabilityScheduledFlightRepo;
+import com.capg.fms.schedule.excepions.InvalidInputException;
+import com.capg.fms.schedule.excepions.SeatsAreNotAvailableException;
 import com.capg.fms.schedule.service.IAvailabilityService;
 
 @SpringBootTest
@@ -14,20 +15,74 @@ class FmsScheduleMsApplicationTests {
 	
 	@Autowired
 	IAvailabilityService availabilityService;
-	
-	@Autowired
-	IAvailabilityScheduleRepo repo;
-	
-	@Autowired
-	IAvailabilityScheduledFlightRepo flightRepo;
-	
-	static ScheduledFlight scheduledFlight;
 
-//	@Test
-//	void contextLoads() {	
-//	}
 	@Test
-	public void testFlightNnumber(){
-		assertEquals(true, availabilityService.validateFlightId(4462336284L));
+	public void testFlightNumberwithLessDigits() throws InvalidInputException{
+	
+		Exception exception = assertThrows(InvalidInputException.class, () -> {
+			 availabilityService.validateFlightId(4567890L);
+		    });
+		    String expectedMessage = "ID should be of 12 digits";
+		    String actualMessage = exception.getMessage();	 
+		    assertTrue(actualMessage.contains(expectedMessage));
 	}
+	
+	@Test
+	public void testFlightNumberwithMoreDigits() throws InvalidInputException{
+		Exception exception = assertThrows(InvalidInputException.class, () -> {
+			 availabilityService.validateFlightId(1234345678907856L);
+		    });
+		 
+		    String expectedMessage = "ID should be of 12 digits";
+		    String actualMessage = exception.getMessage();
+		    assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
+	@Test
+	public void testFlightNumber() {
+		assertEquals("The flight is available", availabilityService.checkScheduledFlightById(446233628412L));
+	}
+	
+	@Test
+	public void testSourceAirport() throws InvalidInputException {
+		Exception exception = assertThrows(InvalidInputException.class, () -> {
+			 availabilityService.checkSource(446233628412L, "Vizag");
+		    });
+		 
+		    String expectedMessage = "Source Airport is not valid";
+		    String actualMessage = exception.getMessage();
+		    assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
+	@Test
+	public void testDestinationAirport() throws InvalidInputException {
+		Exception exception = assertThrows(InvalidInputException.class, () -> {
+			 availabilityService.checkDestination(446233628412L, "Mumbai");
+		    });
+		 
+		    String expectedMessage = "Destination Airport is not valid";
+		    String actualMessage = exception.getMessage();
+		    assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
+	@Test
+	public void testSourceAndDestinationAirport() throws InvalidInputException {
+		Exception exception = assertThrows(InvalidInputException.class, () -> {
+			 availabilityService.checkSourceAndDestination(446233628412L, "Vizag", "Mumbai");
+		    });
+		 
+		    String expectedMessage = "Flight is not available";
+		    String actualMessage = exception.getMessage();
+		    assertTrue(actualMessage.contains(expectedMessage));
+	}
+	
+	@Test
+	public void testAvailableSeats() throws SeatsAreNotAvailableException {
+		assertTrue(availabilityService.checkSeatAvailability(446233628412L, 21));
+	}
+	
+//	@Test
+//	public void testNoAvailableSeats() throws SeatsAreNotAvailableException {
+//		assertFalse(availabilityService.checkSeatAvailability(446233628412L,));
+//	}
 }
