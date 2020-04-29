5b3bc88183c3ce7934cb8ec1a59291b99becd123
Nived Chowdary <nivedchoudary@gmail.com>
2020-04-29 17:35:42 +0530

    J-Unit added

--- fms-schedule-ms/src/test/java/com/capg/fms/schedule/FmsScheduleMsApplicationTests.java
+++ fms-schedule-ms/src/test/java/com/capg/fms/schedule/FmsScheduleMsApplicationTests.java
@@ -1,9 +1,67 @@
 package com.capg.fms.schedule;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.capg.fms.schedule.model.Schedule;
+import com.capg.fms.schedule.model.ScheduledFlight;
+import com.capg.fms.schedule.repository.IScheduleRepo;
+import com.capg.fms.schedule.service.IScheduleService;
 
 @SpringBootTest
 class FmsScheduleMsApplicationTests {
+	
+	@Autowired
+	IScheduleService service;
+	@Autowired
+	IScheduleRepo repo;
+
+	Schedule schedule = new Schedule(101, "DEL", "PAK", LocalDateTime.of(2020, 05, 05, 10, 30, 55),
+			LocalDateTime.of(2020, 05, 03, 05, 10, 55));
+	ScheduledFlight scheduledFlight = new ScheduledFlight(101, 30, 143, schedule);
+	
+	@Test
+	public void validateScheduleFlightById() {
+		
+		assertEquals(repo.getOne(101).getScheduledFlightId(), service.viewScheduledFlight(101).getScheduledFlightId());
+		
+	}
+	
+	@Test
+	public void validateScheduleFlightByFlightNumber() {
+		
+		assertEquals(scheduledFlight.getFlightNumber(), service.viewScheduledFlight(101).getFlightNumber());
+		
+	}
+	@Test
+	public void validateScheduleFlightByAvailableSeats() {
+		
+		assertEquals(scheduledFlight.getAvailableSeats(), service.viewScheduledFlight(101).getAvailableSeats());
+		
+	}
+	@Test
+	public void validateScheduleFlightByScheduleId() {
+		
+		assertEquals(scheduledFlight.getSchedule().getScheduleId(), service.viewScheduledFlight(101).getSchedule().getScheduleId());
+		
+	}
+	@Test
+	public void validateScheduleFlightByScheduleSourceAirport() {
+		
+		assertEquals(scheduledFlight.getSchedule().getSourceAirport(), service.viewScheduledFlight(101).getSchedule().getSourceAirport());
+		
+	}
+	@Test
+	public void validateScheduleFlightByScheduleDestinationAirport() {
+		
+		assertEquals(scheduledFlight.getSchedule().getDestinationAirport(), service.viewScheduledFlight(101).getSchedule().getDestinationAirport());
+		
+	}
 	
 	
 }
